80e32d82701003573034c9a4c93c1f6ad7c6188d
catecodes <catherinedews@gmail.com>
2025-03-05 17:45:20 -0500

    secure vault

---
+++ script.js
@@ -0,0 +1,13 @@
+// create a pop up for user delivering the message and the vault combination numbers
+
+const message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination";
+
+// create equations to find vault combo
+// vault combo numbers are 10 - 40 - 39
+
+const num1 = 9 + 1;
+const num2 = 39 + 1;
+const num3 = 38 + 1;
+
+alert(`${message}: ${num1} ${num2} ${num3}`);
